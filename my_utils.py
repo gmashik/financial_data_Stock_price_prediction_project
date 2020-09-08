afffca4f7dfe0364ae7f8b8dd6f6c85798793354
gmashik <48297994+gmashik@users.noreply.github.com>
2020-09-08 17:32:25 -0400

    Add files via upload

---
+++ my_utils.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import cufflinks as cf
+from plotly import __version__
+import plotly.offline as pyo
+import plotly.graph_objs as go
+from plotly.offline import iplot
+import tensorflow as tf
+def configure_plotly_browser_state():
+  import IPython
+  display(IPython.core.display.HTML('''
+        <script src="/static/components/requirejs/require.js"></script>
+        <script>
+          requirejs.config({
+            paths: {
+              base: '/static/base',
+              plotly:  'https://cdn.plot.ly/plotly-latest.min.js?noext',
+            },
+          });
+        </script>
+        '''))
+  
+def test_mod():
+	print("Successfully imported")
+def plot_series(time, series, format="-", start=0, end=None):
+    plt.plot(time[start:end], series[start:end], format)
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    plt.grid(True)
+def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
+    series = tf.expand_dims(series, axis=-1)
+    dataset = tf.data.Dataset.from_tensor_slices(series)
+    dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
+    dataset = dataset.flat_map(lambda w: w.batch(window_size + 1))
+    dataset = dataset.shuffle(shuffle_buffer)
+    dataset = dataset.map(lambda w: (w[:-1], w[1:]))
+    return dataset.batch(batch_size).prefetch(1)
+def os_model_forecast(model, series, window_size):
+    dataset = tf.data.Dataset.from_tensor_slices(series)
+    dataset = dataset.window(window_size, shift=1, drop_remainder=True)
+    dataset = dataset.flat_map(lambda w: w.batch(window_size))
+    dataset = dataset.batch(32).prefetch(1)
+    osforecast = model.predict(dataset)
+    return osforecast
